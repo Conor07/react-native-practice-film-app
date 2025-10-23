fcf022af47649c4ab95bfa8f0e226e27d1e80090
Conor Talbot <conor@Conors-MacBook-Pro-2.local>
2025-10-23 12:33:12 +0100

    Implemented home page. Still following tutorial

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,5 +3,11 @@ import "./globals.css";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+
+      <Stack.Screen name="film/[id]" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
---
+++ app/film/[id].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const FilmDetails = () => {
+  return (
+    <View>
+      <Text>FilmDetails</Text>
+    </View>
+  );
+};
+
+export default FilmDetails;
